309b64b1bef40e713657743d7ceadac1af4df0ba
Mikhail Danshin <mdanshin@gmail.com>
2022-02-08 09:20:56 +0000

    remove extra code

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Routes, Route } from 'react-router-dom'
-import CreateServiceUser from '../pages/CreateServiceAccForm'
 import MainForm from '../pages/MainForm'
 
 export const useRoutes = () => {
@@ -10,7 +9,6 @@ export const useRoutes = () => {
             <Route path="/sites" element={<h1>Sites</h1>} />
             <Route path="/users" element={<h1>Users</h1>} />
             <Route path="/servers" element={<h1>Servers</h1>} />
-            <Route path="/request" element={<CreateServiceUser />} />
             <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
     )
